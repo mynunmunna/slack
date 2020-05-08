a122fc2acd3c61380f53ed02bbedca69547c33e3
mynunmunna <64220793+mynunmunna@users.noreply.github.com>
2020-05-08 15:26:42 -0400

    Update Maruf.java

--- src/day_1/Maruf.java
+++ src/day_1/Maruf.java
@@ -3,5 +3,7 @@ package day_1;
 public class Maruf {
     public static void main(String[] args) {
         System.out.println("I do not know what to write");
+        
+        System.out.println("Let's pull this ocde to IntelliJ");
     }
 }
